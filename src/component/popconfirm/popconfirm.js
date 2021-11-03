a79ba87691a33d3b7af26078480610b8a574b2e2
쫑탱 <jjojjo141995@gmail.com>
2021-11-03 23:35:27 +0900

    popconfirm 초기작업

--- src/component/popconfirm/popconfirm.js
+++ src/component/popconfirm/popconfirm.js
@@ -1,3 +1,25 @@
-export default class PopConfirm {
+export default class PopConfirm extends HTMLElement{
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open'});
 
+
+  }
+
+  createElement() {
+    this.$container = document.createElement('div');
+
+
+  }
+
+  initStyle() {
+    this.$style = document.createElement('style');
+    this.$style.textContent = `
+      
+    `
+
+    return this.$style;
+  }
+
+  
 }
