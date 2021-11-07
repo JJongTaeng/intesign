9179f08e1cfc6d23cd03675823b1bd5b67d7d742
쫑탱 <jjojjo141995@gmail.com>
2021-11-07 22:49:20 +0900

    Update popconfirm.js

--- src/component/popconfirm/popconfirm.js
+++ src/component/popconfirm/popconfirm.js
@@ -1,4 +1,8 @@
 export default class PopConfirm extends HTMLElement{
+  static get observedAttributes() {
+    return ['okText', 'cancelText', 'style'];
+  }
+
   constructor() {
     super();
     this.attachShadow({ mode: 'open'});
@@ -16,7 +20,22 @@ export default class PopConfirm extends HTMLElement{
         break;
       case 'cancelText':
         break;
+      case 'style':
+        this.updateStyle`${newValue}`;
+        break;
     }
+  }
+
+  updateStyle(style, ...arg) {
+    const raw = style.raw.reduce((prev, current, index) => {
+      if(arg.length > 0) {
+        console.log(index);
+        return prev + arg[index-1] + current
+      } else {
+        return prev + current;
+      }
+    });
+    this.$style.textContent = this.$style.textContent + raw;
   }
 
   initStyle() {
