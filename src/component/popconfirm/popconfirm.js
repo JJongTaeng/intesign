4c90c82022c8e53a8806d66c08d0191401a586f6
쫑탱 <jjojjo141995@gmail.com>
2021-11-11 23:31:47 +0900

    popconfirm 창 UI작업

--- src/component/popconfirm/popconfirm.js
+++ src/component/popconfirm/popconfirm.js
@@ -1,4 +1,4 @@
-import Button from "../form/button";
+import Button from "../form/button.js";
 
 export default class PopConfirm extends HTMLElement{
   static get observedAttributes() {
@@ -11,25 +11,43 @@ export default class PopConfirm extends HTMLElement{
 
     this.createElement();
     this.appendElement();
-
+    this.setClass();
+    this.click();
+    this.shadowRoot.append(this.initStyle(), this.$container);
   }
 
   createElement() {
     this.$container = document.createElement('div');
     this.$message = document.createElement('div');
+    this.$message.textContent = 'Any Message';
     this.$form = document.createElement('div');
 
     this.$cancel = document.createElement('inte-button');
+    Button.setName(this.$cancel, 'Cancel');
+
     this.$ok = document.createElement('inte-button');
+    Button.setName(this.$ok, 'Ok');
+
+
   }
 
   setClass() {
     this.$container.className = 'container';
+    this.$message.className = 'message';
+    this.$form.className = 'form';
+    this.$cancel.className = 'cancel';
+    this.$ok.className = 'ok';
   }
 
   appendElement() {
     this.$form.append(this.$cancel, this.$ok);
     this.$container.append(this.$message, this.$form);
+  }
+
+  click() {
+    this.$container.addEventListener('click', (e) => {
+      console.log(e)
+    })
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
@@ -61,12 +79,32 @@ export default class PopConfirm extends HTMLElement{
     this.$style = document.createElement('style');
     this.$style.textContent = `
       .container {
+        position: aboslute;
+        top: 1rem;
+        right: 0;
+         
         display: flex;
         flex-direction: column;
         position: absolute;
+        background: white;
+        padding: 10px;
+        min-width: 160px;
+        box-shadow: 0px 0px 12px 4px rgba(0, 0, 0, .4);
+        
+      }
+      .message {
+        margin: 10px;
       }
       .form {
+        display: flex;
+        margin-left: auto;
+      }
+      .cancel {
         
+        margin: 4px;
+      }
+      .ok {
+        margin: 4px;
       }
     `
 
--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import Row from "./component/grid/row.js";
 import Modal from './component/modal/modal.js';
 import Message from './component/message/message.js';
 import Input from './component/form/input.js';
+import PopConfirm from "./component/popconfirm/popconfirm.js";
 
 export {
-  Row, Column, Card, Modal, Message, Input
+  Row, Column, Card, Modal, Message, Input, PopConfirm
 }
