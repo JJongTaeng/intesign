f077cd0762f584b63124404e84ab3124c6e1fb54
쫑탱 <jjojjo141995@gmail.com>
2022-01-29 01:31:12 +0900

    테스트 환경 구축

--- src/component/card/Card.test.ts
+++ src/component/card/Card.test.ts
@@ -1,7 +1,12 @@
+import '@testing-library/jest-dom'
+import Card from './Card';
 function sum(a, b) {
   return a + b;
 }
 
 test('adds 1 + 2 to equal 3', () => {
+  let card = new Card();
+  card.setBody('Hello');
+
   expect(sum(1, 2)).toBe(3);
 });
