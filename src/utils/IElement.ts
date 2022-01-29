a40df4d8f29129142d14e48b7a2dcd0ca66705c1
쫑탱 <jjojjo141995@gmail.com>
2022-01-29 21:46:10 +0900

    IElement 유틸 클래스 생성

---
+++ src/utils/IElement.ts
@@ -0,0 +1,38 @@
+interface IEInterface {
+  setAttribute(name: string, attribute: string): this;
+  append(nodes: string | Node[]): this;
+  clearChildren(): this;
+  appendChild(node: Node): this;
+}
+
+export default class IElement<T extends HTMLElement> implements IEInterface{
+  private readonly $element: T;
+
+  constructor(tag: keyof HTMLElementTagNameMap) {
+    this.$element = document.createElement(tag) as T;
+  }
+
+  setAttribute(name, attribute) {
+    this.$element.setAttribute(name, attribute);
+    return this;
+  }
+
+  append(...nodes) {
+    this.$element.append(...nodes);
+    return this;
+  }
+
+  appendChild(node) {
+    this.$element.append(node);
+    return this;
+  }
+
+  getElement(): T {
+    return this.$element;
+  }
+
+  clearChildren() {
+    this.$element.replaceChildren();
+    return this;
+  }
+};
